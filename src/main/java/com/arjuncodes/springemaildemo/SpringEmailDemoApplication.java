5c557f5781ec0b47c386256ee1b16837ef4283f6
Ariel Soares <ariel.sfranco@gmail.com>
2024-03-17 19:38:20 -0300

    Projeto criado e funcionando

---
+++ src/main/java/com/arjuncodes/springemaildemo/SpringEmailDemoApplication.java
@@ -0,0 +1,26 @@
+package com.arjuncodes.springemaildemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import javax.mail.MessagingException;
+
+@SpringBootApplication
+public class SpringEmailDemoApplication {
+
+	@Autowired
+	private EmailSenderService senderService;
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmailDemoApplication.class, args);
+	}
+	@EventListener(ApplicationReadyEvent.class)
+	public void triggerMail() throws MessagingException {
+		senderService.sendSimpleEmail("antoniohenriquep@outlook.com",
+				"Email automático mandado por rotina",
+				"A partir de hoje você receberá um email diário lhe lembrando do quão fuleiro é");
+
+	}
+}
